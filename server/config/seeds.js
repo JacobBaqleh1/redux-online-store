9f73e873994ff1105ee92111d87009df4b466362
Jacob Baqleh <baqlehjacob1@gmail.com>
2025-01-04 17:50:58 -0800

    update json

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,5 +1,5 @@
 const db = require('./connection');
-const { User, Product, Category } = require('../models');
+const { Product, Category } = require('../models'); // Removed User import
 
 db.once('open', async () => {
   await Category.deleteMany();
@@ -127,29 +127,6 @@ db.once('open', async () => {
   ]);
 
   console.log('products seeded');
-
-  await User.deleteMany();
-
-  await User.create({
-    firstName: 'Pamela',
-    lastName: 'Washington',
-    email: 'pamela@testmail.com',
-    password: 'password12345',
-    orders: [
-      {
-        products: [products[0]._id, products[0]._id, products[1]._id]
-      }
-    ]
-  });
-
-  await User.create({
-    firstName: 'Elijah',
-    lastName: 'Holt',
-    email: 'eholt@testmail.com',
-    password: 'password12345'
-  });
-
-  console.log('users seeded');
 
   process.exit();
 });
